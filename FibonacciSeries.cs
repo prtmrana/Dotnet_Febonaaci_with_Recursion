07aac34ae947d37131aed34e8f80ea43e21d0cbb
PREETAM SINGH RANA <119283406+prtmrana@users.noreply.github.com>
2024-01-23 18:31:16 +0530

    Add files via upload

---
+++ FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class FibonacciSeries
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of terms in Fibonacci series:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
