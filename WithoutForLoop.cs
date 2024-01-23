efb536242a62cc13532069ae8d7a13a2df5ccd26
PREETAM SINGH RANA <119283406+prtmrana@users.noreply.github.com>
2024-01-23 18:30:50 +0530

    Add files via upload

---
+++ WithoutForLoop.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class WithoutForLoop
+
+
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number of terms in Fibonacci series:");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci Series:");
+
+        PrintFibonacciSeries(n, 0);
+    }
+
+    static void PrintFibonacciSeries(int n, int currentIndex)
+    {
+        if (currentIndex < n)
+        {
+            Console.Write(Fibonacci(currentIndex) + " ");
+            PrintFibonacciSeries(n, currentIndex + 1);
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
+
+
